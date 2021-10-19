ddd71d135f53c672667298088196946603d64324
Anton Andriievskyi <anton.andriievskyi@toptal.com>
2021-10-19 11:52:19 +1030

    Add "void" literal.

--- _dev/shared.js
+++ _dev/shared.js
@@ -12,7 +12,7 @@ function jai_language_definition() {
         keywords: {
             $pattern: /([_a-z][a-z0-9_]+|---)/,
             keyword: ['return', 'struct', 'if', 'ifx', 'else', 'case', 'for', 'while', 'break', 'using', 'enum', 'new', 'defer', 'no_check', 'cast' ],
-            literal: ['false', 'true', 'null', '---',],
+            literal: ['false', 'true', 'null', 'void', '---',],
             built_in: ['Any', 'Type',],
             type: ['u8', 'u16', 'u32', 'u64', 's8', 's16', 's32', 's64', 'int', 'float', 'float32', 'float64', 'string'],
         },
